6dc0d72faf81075c75fedb6b5f3937ea6b82c807
mnmonherdene <mnmonherdene1234@gmail.com>
2022-11-03 16:11:24 +0800

    local

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ builder.Services.AddDbContext<DatabaseContext>(options => options.UseNpgsql(buil
 
 var app = builder.Build();
 
-app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true).AllowCredentials());
+app.UseCors(x => x.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true).AllowCredentials().WithOrigins("https://localhost:3000", "http://localhost:3000"));
 
 app.UseHttpsRedirection();
 
